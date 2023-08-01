bfcbaca3005b3eef63271413e3f81d1eddb587ee
rohitg247 <106257406+rohitg247@users.noreply.github.com>
2023-08-01 11:00:52 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        Welcome to my portfolio website! I am a passionate and driven web developer
+        with a strong foundation in HTML, CSS, and JavaScript.
+        With hands-on experience in technologies like ReactJS, NextJS, and WordPress,
+        I have successfully built engaging and responsive websites.
+        I am adept at leveraging APIs, connecting to databases, and implementing authentication systems.
+        During my internship at Shopify and .Liquid, I gained valuable insights into e-commerce and web application development.
+        With a keen eye for detail and a commitment to delivering exceptional results,
+        I am ready to take on new challenges and contribute to the success of your projects.
+        Explore my portfolio to witness my skills and creativity in action. Let's collaborate and bring your ideas to life!
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, 'about');
